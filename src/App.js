3e03735cd6a0fe051078522c06f9233933667848
Mahardicka <mahardicka.nur@sinbad.co.id>
2023-02-28 18:17:05 +0700

    eartch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "./assets/Planet";
+import { Airplane } from "./assets/Airplane";
+import { Cloud } from "./assets/Cloud";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas style={{ height: "100vh", backgroundColor: "#11101B" }}>
+      <OrbitControls
+        keys={{
+          BOTTOM: "ArrowDown",
+          UP: "ArrowUp",
+          LEFT: "ArrowLeft",
+          RIGHT: "ArrowRight",
+        }}
+      />
+      <scene>
+        <fog color="#11101B" near={0.1} far={1000} />
+        <ambientLight color="rgb(72, 64, 104)" intensity={1} />
+        <Planet />
+        <Airplane />
+        <Cloud castShadow />
+        <axesHelper />
+        <pointLight
+          color="#1BFFEF"
+          position={[0, 8, -16]}
+          intensity={2}
+          castShadow
+        />
+        <directionalLight
+          shadow={{ mapSize: { height: 2048, width: 2048 } }}
+          position={[[0, 2, 4]]}
+          intensity={1}
+          castShadow
+        />
+      </scene>
+    </Canvas>
   );
 }
 
